b54e877cdceb806c4d9686f0fa2382296e9ce0e5
TimLin <hughes0227@gmail.com>
2024-07-24 13:29:38 +0800

    Update

--- js/main.js
+++ js/main.js
@@ -93,19 +93,6 @@
         }
     });
 
-    fetch('./data/prof.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-    });
 
     
 })(jQuery);
